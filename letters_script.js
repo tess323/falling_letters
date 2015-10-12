7dbd492da28c146de0a109f8c2a92a5a82ceeebc
tess323 <tess323@gmail.com>
2015-10-12 14:43:44 -0700

    letters generate and fall

--- letters_script.js
+++ letters_script.js
@@ -1,3 +1,14 @@
+// random number generator to dictate span spacing
+function randomNumber(max){
+	var randomNum = Math.random();
+	var numToMax = randomNum * max;
+	var numFloored = Math.floor(numToMax);
+
+	return numFloored;
+	console.log(numFloored);
+
+}
+
 // random letter generator
 function letterGen(len)
 {
@@ -9,20 +20,39 @@ function letterGen(len)
         text += charset.charAt(Math.floor(Math.random() * charset.length));
 
     return text;
+    console.log(letterGen);
 }
 
-// call letterGen for 1 letter
-function randomLetter(){
-	letterGen(1);
-
-}
 
 // Create a span to put that letter in
+function createSpan(){
+	var newSpan = $('<span/>');
+	newSpan.css('position','absolute');
+	newSpan.css('top',0);
+	randomPercent = randomNumber(100)+'%';
+	newSpan.css('left', randomPercent);
+	newSpan.text(letterGen(1))
+
+	newSpan.appendTo('body');
+
+	return newSpan;
+	console.log(newSpan);
+
+}
+
+// make letters fall using animation 
+function lettersFall(){
+	var letterSpan = createSpan()
+	letterSpan.animate({'top':'95%'}, 5000)
+}
+// make letters disappear when you strike the key
 
 // function 
 
 $(document).ready(function(){
-	randomLetter();
+	letterGen();
+	// createSpan();
+	lettersFall();
 	
 });
 
